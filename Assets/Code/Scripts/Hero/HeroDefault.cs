74a0ef2b1ad20a78fe8838a78358a6834e6bce19
David Maidenberg <david.maidenberg@epitech.eu>
2019-04-23 10:58:02 -0700

    Add a UnitBrain for the Hero

---
+++ Assets/Code/Scripts/Hero/HeroDefault.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Brains/HeroDefault")]
+public class HeroDefault : UnitBrain
+{
+    public UnitSet targets;
+
+    public override void Assign(UnitSet set) { targets = set; }
+
+    public override void Initialize(Unit unit, Unit enemyHero)
+    {
+        base.Initialize(unit, enemyHero);
+        unit.GetComponent<UnitHealth>().alive = true;
+    }
+
+    public override void Think(Unit unit)
+    {
+        if (currentTarget == null && !SelectTarget(unit))
+            return;
+        if (unitAttack.atkReload <= 0f && unitAttack.Attack(currentTarget))
+            currentTarget = null;
+    }
+
+    private bool SelectTarget(Unit unit)
+    {
+        if (targets.items.Count > 0)
+        {
+            foreach (Unit element in targets.items)
+                if (element != null && element.isActiveAndEnabled && element.tag != unit.tag)
+                {
+                    currentTarget = element;
+                    return true;
+                }
+        }
+        return false;
+    }
+}
