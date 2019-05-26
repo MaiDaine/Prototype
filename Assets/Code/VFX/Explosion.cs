b8a4665b95b8560f2b8ba2073c7d3dd3001a72cb
David Maidenberg <david.maidenberg@epitech.eu>
2019-05-25 19:53:32 -0700

    Add basic VisualEffect to test new setup

---
+++ Assets/Code/VFX/Explosion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class Explosion : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("StartExplosion", 1.5f);
+    }
+
+    private void StartExplosion()
+    {
+        GetComponent<VisualEffect>().SendEvent("OnExplosion");
+        Invoke("EndExplosion", 1f);
+    }
+
+    private void EndExplosion()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
