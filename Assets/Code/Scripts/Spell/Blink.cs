93c3e47975caf8d6baf830e06cba21ed36130b72
David Maidenberg <david.maidenberg@epitech.eu>
2019-06-02 10:50:04 -0700

    Add guard to prevent errors when blink is used against a wall

--- Assets/Code/Scripts/Spell/Blink.cs
+++ Assets/Code/Scripts/Spell/Blink.cs
@@ -29,6 +29,12 @@ namespace Prototype
                 NavMesh.FindClosestEdge(unit.transform.position + distance * unit.transform.forward, out hit, NavMesh.AllAreas);
                 if (hit.position.x != Mathf.Infinity && hit.position.y != Mathf.Infinity)
                     unit.transform.position = new Vector3(hit.position.x, unit.transform.position.y, hit.position.z);
+                else if (NavMesh.Raycast(unit.transform.position, unit.transform.position + distance * unit.transform.forward, out hit, NavMesh.AllAreas))
+                {
+                    NavMesh.FindClosestEdge(hit.position, out hit, NavMesh.AllAreas);
+                    if (hit.position.x != Mathf.Infinity && hit.position.y != Mathf.Infinity)
+                        unit.transform.position = new Vector3(hit.position.x, unit.transform.position.y, hit.position.z);
+                }
             }
             base.Effect();
         }
