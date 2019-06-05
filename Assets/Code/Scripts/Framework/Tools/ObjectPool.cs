e9f49fad313d662efcc7bff867f791161522e3b9
David Maidenberg <david.maidenberg@epitech.eu>
2019-06-04 22:54:40 -0700

    Add ObjectPool and launcher class to handler bulletHell mmechanic

---
+++ Assets/Code/Scripts/Framework/Tools/ObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        public ObjectPoolItem item;
+        private List<GameObject> pooledObjects;
+
+        void Start()//TODO LOAD
+        {
+            pooledObjects = new List<GameObject>();
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+
+        public GameObject GetPooledObject()
+        {
+            for (int i = 0; i < pooledObjects.Count; i++)
+                if (!pooledObjects[i].activeInHierarchy)
+                    return pooledObjects[i];
+            GameObject obj = (GameObject)Instantiate(item.objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+            return obj;
+        }
+    }
+}
---
+++ Assets/Code/Scripts/Framework/Tools/ObjectPoolItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prototype
+{
+    [Serializable]
+    public class ObjectPoolItem
+    {
+        public GameObject objectToPool;
+        public int amountToPool;
+    }
+}
