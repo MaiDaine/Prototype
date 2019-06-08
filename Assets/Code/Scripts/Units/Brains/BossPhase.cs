21eea0c1bdee87b568e3b9f035f7ce187a33f6a6
David Maidenberg <david.maidenberg@epitech.eu>
2019-06-08 14:42:53 -0700

    Add Structs to store boss phases informations

---
+++ Assets/Code/Scripts/Units/Brains/BossPhase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Prototype
+{
+    [Serializable]
+    public struct BossPhaseStatus
+    {
+        public UnitStatus.statusTypes type;
+        public float power;
+        public float duration;
+    }
+
+    [Serializable]
+    public struct BossPhase
+    {
+        public BossPhaseStatus[] phaseStatus;
+        public SpellLauncher[] launchers;
+        public int activeLaunchers;
+        public GameEvent[] mapEvents;
+    }
+}
